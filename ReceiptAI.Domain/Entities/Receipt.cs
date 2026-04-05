3d7a9cf5ceb8c779b0558912a1bdf6dda393897a
Antonio Lopes da Cruz <tonycruz1@hotmail.com>
2026-04-05 15:34:57 +0100

    Add Receipt domain entity

---
+++ ReceiptAI.Domain/Entities/Receipt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReceiptAI.Domain.Entities;
+
+public class Receipt
+{
+	public Guid Id { get; set; }
+	public string MerchantName { get; set; } = string.Empty;
+	public DateTime PurchaseDate { get; set; }
+	public decimal TotalAmount { get; set; }
+	public string Currency { get; set; } = "GBP";
+	public string Category { get; set; } = "Other";
+	public string ImageUrl { get; set; } = string.Empty;
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+	public string ImagePublicId { get; set; } = string.Empty;
+}
